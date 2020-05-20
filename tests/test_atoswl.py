319c70d7ccc90f4fedac2652060c4b5527230320
Zaiqiao Meng <zqmeng@aliyun.com>
2020-05-20 11:30:33 +0100

    change default setting

--- tests/test_atoswl.py
+++ tests/test_atoswl.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("../")
 
 from beta_rec.datasets.atoswl import AtosWl
@@ -10,15 +11,3 @@ if __name__ == "__main__":
     interactions = dataset.load_interaction()
     print(interactions.head())
 
-#     # use filter_user_item_order() to make the dataset smaller, just for test
-#     interactions = filter_user_item_order(interactions, 1, 1, 1)
-
-#     # use n_test=1 to save time as well
-#     dataset.make_temporal_basket_split(interactions)
-
-#     dataset.load_leave_one_basket(n_test=1)
-#     dataset.load_leave_one_out(n_test=1)
-#     dataset.load_random_basket_split(n_test=1)
-#     dataset.load_random_split(n_test=1)
-#     dataset.load_temporal_basket_split(n_test=1)
-#     train_data, valid_data_li, test_data_li = dataset.load_temporal_split(n_test=1)
